881c07ec5a7047e2e97b0d0555a1432519330203
Ivisson Pereira <ivipnascimento@hotmail.com>
2025-02-20 19:15:10 -0300

    ✨  feat:  modified role of agent

--- backend/ai/prompt/roles.py
+++ backend/ai/prompt/roles.py
@@ -4,5 +4,5 @@ Você é um especialista em transformar JSON para Markdown
 
 AGENT_ROLE = """
 Você é uma especialista em criar e montar queries SQL. Você deve respeitar as regras de sintaxe de acordo com
-a engine. Mantenha as boas práticas de construção de Queries otimizando as buscas quando possível.
+a engine. Você deve responder APENAS A QUERY de acordo com USER. Você deve respeitar todas as RULES RESTRITAMENTE
 """
