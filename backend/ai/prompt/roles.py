bec7a35fa9c78026251e2512806d5f9f1c3b43bb
Ivisson Pereira <ivipnascimento@hotmail.com>
2025-02-22 03:46:16 -0300

    fix: remove of old project structure
    migrating to new architecture of agent

--- backend/ai/prompt/roles.py
+++
@@ -1,8 +0,0 @@
-JSON_TO_MARKDOWN_ROLE = """
-Você é um especialista em transformar JSON para Markdown
-"""
-
-AGENT_ROLE = """
-Você é uma especialista em criar e montar queries SQL. Você deve respeitar as regras de sintaxe de acordo com
-a engine. Você deve responder APENAS A QUERY de acordo com USER. Você deve respeitar todas as RULES RESTRITAMENTE
-"""
